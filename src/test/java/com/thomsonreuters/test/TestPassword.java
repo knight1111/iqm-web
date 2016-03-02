c7d0f5607d7ad1e1aef164e69ec8a3aa69c5a481
Wenjia Wu <wenjia.wu@thomsonreuters.com>
2016-03-02 18:11:47 +0800

    Added support for JUnit WebAppConfiguration

--- src/test/java/com/thomsonreuters/test/TestPassword.java
+++ src/test/java/com/thomsonreuters/test/TestPassword.java
@@ -1,36 +1,25 @@
 package com.thomsonreuters.test;
 
-import javax.annotation.Resource;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.thomsonreuters.modules.am.domain.User;
 import com.thomsonreuters.modules.am.service.PasswordHelper;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring-persistence.xml" })
 public class TestPassword {
-	
-	@Resource
-	private PasswordHelper passwordHelper;
 	
 	@Test
 	public void test1() {
 		
+		PasswordHelper passwordHelper = new PasswordHelper();
+		
 		String username = "admin";
 		
 		User u = new User(-1, username);
-		u.setPassword(null);
+		u.setPassword("123456");
 		u.setSalt("1");
 		passwordHelper.encryptPassword(u);
 		
 		System.err.println("------------------->"+u.getPassword());
-		
-		username = null;
-		System.err.println("------------------->"+"55555".equals(username));
 	}
 
 }
--- src/test/java/com/thomsonreuters/testMybatis/TestMybatis.java
+++ src/test/java/com/thomsonreuters/testMybatis/TestMybatis.java
@@ -1,27 +1,37 @@
 package com.thomsonreuters.testMybatis;
 
-import javax.annotation.Resource;
-
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import com.thomsonreuters.common.web.bean.ResultBean;
 import com.thomsonreuters.modules.am.domain.User;
 import com.thomsonreuters.modules.am.service.IUserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration(locations = { "classpath:spring-persistence.xml" })
 public class TestMybatis {
 	private static Logger logger = Logger.getLogger(TestMybatis.class);
 
-	@Resource
+	@Autowired
 	private IUserService userService;
+	
+	@Autowired
+	private MockHttpServletRequest request;
+
+	@Before
+	public void before() {
+		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(
+				request));
+	}
 
 	@Test
 	public void test() {
@@ -32,18 +42,18 @@ public class TestMybatis {
 
 	@Test
 	public void testUserSave() {
-		User user = userService.getByUsername("222");
+		User user = userService.getByUsername("test1");
 		if (user != null) {
 			user.setName("NewTwo");
 			logger.info(userService.save(user).getIsSuccess());
 		}
 	}
 
-//	@Test
-//	public void testUserDelete() {
-//		User user = userService.getByUsername("111");
-//		if (user != null) {
-//			logger.info(userService.delete(user).getIsSuccess());
-//		}
-//	}
+	// @Test
+	// public void testUserDelete() {
+	// User user = userService.getByUsername("111");
+	// if (user != null) {
+	// logger.info(userService.delete(user).getIsSuccess());
+	// }
+	// }
 }
